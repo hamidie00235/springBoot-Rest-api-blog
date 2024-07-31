257e207108491be736f9b89144a97c1739725183
hamidie ahmat <ahmathisseinh@gmail.com>
2024-08-01 01:54:41 +0300

    first commit

---
+++ src/main/java/com/springbootblogproject/SpringbootRestApiBlogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springbootblogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApiBlogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApiBlogProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootblogproject/SpringbootRestApiBlogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootblogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiBlogProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
