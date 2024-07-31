66fc55ea763d32057faf6653ee99919d7133dd40
hamidie ahmat <ahmathisseinh@gmail.com>
2024-08-01 02:07:11 +0300

    Creating JPA Repository -PostRepository

---
+++ src/main/java/com/springbootblogproject/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.springbootblogproject.repository;
+
+import com.springbootblogproject.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
