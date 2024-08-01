759e6c1f1bfe2873b0ae8d5a3950edb3458aaed8
hamidie ahmat <ahmathisseinh@gmail.com>
2024-08-01 19:36:18 +0300

    Creating DTO Class - PostDto

---
+++ src/main/java/com/springbootblogproject/payload/PostDto.java
@@ -0,0 +1,11 @@
+package com.springbootblogproject.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+}
