d1ffe8dc0c6f424ebe3130d6314f547453ee367d
hamidie ahmat <ahmathisseinh@gmail.com>
2024-08-12 22:47:52 +0300

    Ordering in Sorting API - ASC and DESC

--- src/main/java/com/springbootblogproject/service/impl/PostServiceImp.java
+++ src/main/java/com/springbootblogproject/service/impl/PostServiceImp.java
@@ -38,9 +38,11 @@ public class PostServiceImp implements PostService {
     }
 
     @Override
-    public PostResponse getAllPosts(int pageNo,int pageSize,String sortBy) {
+    public PostResponse getAllPosts(int pageNo,int pageSize,String sortBy,String sortDir) {
         // create Pageable instance
-       Pageable pageable =PageRequest.of(pageNo,pageSize, Sort.by(sortBy));
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+                :Sort.by(sortBy).descending();
+       Pageable pageable =PageRequest.of(pageNo,pageSize,sort);
 
         Page<Post> posts = postRepository.findAll(pageable);
         // get content for page object
